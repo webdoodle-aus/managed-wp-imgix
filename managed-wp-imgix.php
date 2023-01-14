987b5d26e15aa5fcdae58cc2d63e0cd98f66518a
Asad Manzoor <asad33manzoor@gmail.com>
2023-01-14 19:01:03 +0100

    Initial commit

---
+++ managed-wp-imgix.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Managed WP ImgIX
+ * Version: 0.0.1
+ * Description: Managed Wordpress ImgIX integration
+ * Author: Ryan Soury | Web Doodle
+ * Author URI: https://www.webdoodle.com.au/
+ */
