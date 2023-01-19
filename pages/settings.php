50475a20a65b80193a37ee4c1b47e58f9aad0d59
Muzzammil Hussain <muzzammilz@gmail.com>
2023-01-19 20:54:00 +0500

    Fixed poweredby overlapping

--- pages/settings.php
+++ pages/settings.php
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 </div>
 
-<footer style="position:absolute; padding-top: 500px">Powered by <a href="https://webdoodle.com.au/">Web Doodle</a></footer>
+<div class="alignleft"><p>Powered by <a href="https://webdoodle.com.au/">Web Doodle</a></p></div>
 
 <script type="text/javascript">
     function handle_wp_imgix_settings_display() {
